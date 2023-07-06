fdc9b94ec03d3f14ef18d758eed64572765ea3d6
JuliaTarasenko00 <juliatarase17@gmail.com>
2023-07-06 14:40:41 +0300

    hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,57 @@
+import { useState, useEffect } from 'react';
+import { nanoid } from 'nanoid/non-secure';
+import { ContactsForm } from './ContactForm/ContactForm';
+import Filter from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+export { ContactList } from './ContactList/ContactList';
+
+const KEY = 'contacts';
+
 export const App = () => {
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(window.localStorage.getItem(KEY)) ?? []
+  );
+  const [filter, setFilter] = useState('');
+
+  const onAddPhoneBook = contact => {
+    const namePhone = contacts.find(({ name }) => contact.name === name);
+    if (namePhone) {
+      alert(`${contact.name} is already in contacts.`);
+      return;
+    }
+    const contactList = {
+      ...contact,
+      id: nanoid(),
+    };
+    setContacts(prevState => [contactList, ...prevState]);
+  };
+
+  const onRemoveContact = contactId => {
+    setContacts(prevState =>
+      prevState.filter(contact => contact.id !== contactId)
+    );
+  };
+
+  const filterPhone = e => {
+    setFilter(e.target.value.toLowerCase());
+  };
+  useEffect(() => {
+    window.localStorage.setItem(KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter)
+  );
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <h1>Phonebook</h1>
+      <ContactsForm onAddPhoneBook={onAddPhoneBook} />
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={filterPhone} />
+      <ContactList
+        filteredContacts={filteredContacts}
+        onClick={onRemoveContact}
+      />
+    </>
   );
 };
